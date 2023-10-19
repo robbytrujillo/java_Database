ce7218c1d924d20ae9d8567d1578a28c47a95901
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-20 06:50:52 +0700

    Java Database | DataSource - HikariCP

--- src/test/java/programmer/zaman/now/database/ConnectionPoolTest.java
+++ src/test/java/programmer/zaman/now/database/ConnectionPoolTest.java
@@ -1,9 +1,29 @@
 package programmer.zaman.now.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
 
 public class ConnectionPoolTest {
 
     @Test
+    void testHikariCP () { // konfigurasi HikariCP
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost/belajar_java_database");
+        config.setUsername("root");
+        config.setPassword("");
 
+        // configuration connection pool
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setIdleTimeout(60_000);
+        config.setMaxLifetime(10 * 60_000);
+
+        // membuat connection pool
+        HikariDataSource dataSource = new HikariDataSource(config);
+
+        // membuat connection
+        dataSource.getConnection();
+    }
 }
