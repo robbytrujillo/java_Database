c708a89375bf2a8f99ae4aea520c6cc9d39133d2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-22 06:37:17 +0700

    Java Database | Statement

--- src/test/java/programmer/zaman/now/database/StatementTest.java
+++ src/test/java/programmer/zaman/now/database/StatementTest.java
@@ -2,10 +2,21 @@ package programmer.zaman.now.database;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class StatementTest {
 
     @Test
-    void testCreateStatement() {
+    void testCreateStatement() throws SQLException {
 
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+        Statement statement = connection.createStatement();
+
+        statement.close();
+
+        connection.close();
     }
 }
