f9c82f84a5eea012b8952bbde451a47f463d6d70
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-22 22:55:43 +0700

    Java Database | Statement

--- src/test/java/programmer/zaman/now/database/StatementTest.java
+++ src/test/java/programmer/zaman/now/database/StatementTest.java
@@ -3,6 +3,7 @@ package programmer.zaman.now.database;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,6 +15,66 @@ public class StatementTest {
         Connection connection = ConnectionUtil.getDataSource().getConnection();
 
         Statement statement = connection.createStatement();
+
+        statement.close();
+
+        connection.close();
+    }
+
+    @Test
+    void testExecuteUpdate() throws SQLException {
+
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+        Statement statement = connection.createStatement();
+
+        String sql = """
+                INSERT INTO customers(id, name, email)
+                VALUES ('kusuma', 'Kusuma', 'kusuma@mail.com')
+                """;
+        int update = statement.executeUpdate(sql);
+
+        System.out.println(update);
+
+        statement.close();
+
+        connection.close();
+    }
+
+    @Test
+    void testExecuteDelete() throws SQLException {
+
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+        Statement statement = connection.createStatement();
+
+        String sql = """
+               DELETE FROM customers
+                """;
+        int update = statement.executeUpdate(sql);
+
+        System.out.println(update);
+
+        statement.close();
+
+        connection.close();
+    }
+
+@Test
+
+    void testExecuteQuery() throws SQLException {
+
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+        Statement statement = connection.createStatement();
+
+        String sql = """
+               SELECT * FROM customers
+                """;
+
+    ResultSet resultSet = statement.executeQuery(sql);
+
+        resultSet.close();
 
         statement.close();
 
