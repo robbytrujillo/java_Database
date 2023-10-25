6724232894e370b8102eb50d185f0617cd4964a7
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-25 14:38:15 +0700

    Java Database | Prepare Statement

--- src/test/java/programmer/zaman/now/database/PrepareStatementTest.java
+++ src/test/java/programmer/zaman/now/database/PrepareStatementTest.java
@@ -3,6 +3,7 @@ package programmer.zaman.now.database;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class PrepareStatementTest {
@@ -10,7 +11,16 @@ public class PrepareStatementTest {
     @Test
     void testPrepareStatement() throws SQLException {
         Connection connection = ConnectionUtil.getDataSource().getConnection();
+        String username = "admin', #";
+        String password = "admin";
 
+        String sql = "SELECT * FROM admin WHERE username = ? AND password = ? ";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+
+        preparedStatement.close();
         connection.close();
     }
 }
