fecd4b87da62f71238d7e3ad7b5572ff1f87d443
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-31 10:50:54 +0700

    Java Database | Repositoy Pattern

---
+++ src/test/java/programmer/zaman/now/database/RepositoryTest.java
@@ -0,0 +1,52 @@
+package programmer.zaman.now.database;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import programmer.zaman.now.database.entity.Comment;
+import programmer.zaman.now.database.repoisitory.CommentRepository;
+import programmer.zaman.now.database.repoisitory.CommentRepositoryImpl;
+
+import java.util.List;
+public class RepositoryTest {
+
+    private CommentRepository commentRepository;
+
+    @BeforeEach
+    void setUp() {
+        commentRepository = new CommentRepositoryImpl();
+    }
+
+    @Test
+    void testInsert() {
+        Comment comment = new Comment("robbyikusuma@gmail.com", "halo");
+        commentRepository.insert(comment);
+
+        Assertions.assertNotNull(comment.getId());
+        System.out.println(comment.getId());
+    }
+
+    @Test
+    void testFindById() {
+        Comment comment = commentRepository.findById(3307);
+        Assertions.assertNotNull(comment);
+        System.out.println(comment.getId());
+        System.out.println(comment.getEmail());
+        System.out.println(comment.getComment());
+
+        Comment notFound = commentRepository.findById(10000000);
+        Assertions.assertNull(notFound);
+    }
+
+    @Test
+    void testFindAll() {
+        List<Comment> comments = commentRepository.findAll();
+        System.out.println(comments.size());
+    }
+
+    @Test
+    void testFindByEmail() {
+         List<Comment> comments = commentRepository.findAllByEmail("robbyikusuma@gmail.com@mail.com");
+        System.out.println(comments.size());
+    }
+}
